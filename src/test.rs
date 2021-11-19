123e048862ab6b07100790815293fd54e61999c8
Rüdiger Klaehn <rklaehn@protonmail.com>
2021-11-19 16:43:33 +0100

    Add proptest tests

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,7 @@
+use std::collections::BTreeSet;
+
 use crate::{EarlyOut, MergeOperation, MergeStateRead};
+use proptest::prelude::*;
 
 struct VecMergeState<'a, T> {
     a: std::slice::Iter<'a, T>,
@@ -111,6 +114,16 @@ impl<'a, T: Ord + Copy> MergeOperation<BoolMergeState<'a, T>> for Intersects {
     }
 }
 
+fn arb_sorted_vec() -> BoxedStrategy<Vec<u8>> {
+    any::<Vec<u8>>()
+        .prop_map(|mut v| {
+            v.sort();
+            v.dedup();
+            v
+        })
+        .boxed()
+}
+
 #[test]
 fn smoke() {
     let a = vec![1, 2, 3, 4];
@@ -136,4 +149,67 @@ fn smoke() {
     };
     Intersects.merge(&mut s);
     assert_eq!(s.r, true);
+}
+
+fn std_set_union(a: Vec<u8>, b: Vec<u8>) -> Vec<u8> {
+    let mut r = BTreeSet::new();
+    r.extend(a.into_iter());
+    r.extend(b.into_iter());
+    r.into_iter().collect()
+}
+
+fn std_set_intersection(a: Vec<u8>, b: Vec<u8>) -> Vec<u8> {
+    let a: BTreeSet<u8> = a.into_iter().collect();
+    let b: BTreeSet<u8> = b.into_iter().collect();
+    a.intersection(&b).cloned().collect()
+}
+
+fn std_set_intersects(a: Vec<u8>, b: Vec<u8>) -> bool {
+    let a: BTreeSet<u8> = a.into_iter().collect();
+    let b: BTreeSet<u8> = b.into_iter().collect();
+    a.intersection(&b).next().is_some()
+}
+
+proptest! {
+    #[test]
+    fn union(
+        a in arb_sorted_vec(),
+        b in arb_sorted_vec(),
+    ) {
+        let mut s = VecMergeState {
+            a: a.iter(),
+            b: b.iter(),
+            r: Default::default(),
+        };
+        Union.merge(&mut s);
+        prop_assert_eq!(s.r, std_set_union(a, b));
+    }
+
+    #[test]
+    fn intersection(
+        a in arb_sorted_vec(),
+        b in arb_sorted_vec(),
+    ) {
+        let mut s = VecMergeState {
+            a: a.iter(),
+            b: b.iter(),
+            r: Default::default(),
+        };
+        Intersection.merge(&mut s);
+        prop_assert_eq!(s.r, std_set_intersection(a, b));
+    }
+
+    #[test]
+    fn intersects(
+        a in arb_sorted_vec(),
+        b in arb_sorted_vec(),
+    ) {
+        let mut s = BoolMergeState {
+            a: a.iter(),
+            b: b.iter(),
+            r: Default::default(),
+        };
+        Intersects.merge(&mut s);
+        prop_assert_eq!(s.r, std_set_intersects(a, b));
+    }
 }
